41a3418b1aae2e44b852e058c097b32b7488aa8e
Dom <fknbab@pm.me>
2025-03-13 01:22:45 -0700

    Audit-1

--- main-site/astro.config.mjs
+++ main-site/astro.config.mjs
@@ -1,13 +1,15 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
 
 export default defineConfig({
   integrations: [react(), tailwind()],
   vite: {
+    css: {
+      postcss: "../../postcss.config.cjs", // Relative path from each subproject
+    },
     optimizeDeps: {
-      include: ['three'],
+      include: ["three"],
     },
   },
 });
